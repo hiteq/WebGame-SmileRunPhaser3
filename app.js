2a04f9a1c95249e6a9587bf851fa3803c3a24865
Heetaek Woo <hiiteq@gmail.com>
2020-01-08 10:38:53 +0900

    이미지변경

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+console.clear();
+import Phaser from "phaser";
+
 //game balance
 var GRAVITY = 0.6;
 var JUMP = 10;
@@ -10,59 +13,31 @@ var position_y = [40, 60, 120];
 
 //usage
 var player;
+var ground;
 var block;
 var box_0, box_1, box_2, box_3;
 var bg1, bg2, bg3;
 var heart;
 
-//Data URI image
-var image_box_a_1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAeAgMAAAAjs7ROAAAACVBMVEXiPVXwSVv/eJl0VodgAAAANElEQVR4AWNgDIUBHMwQVvxMKpkQtmoVdiZcbdTUldiZYLVozFVITITaVYRNiJqKnUnQFwAym2QtaZGhGAAAAABJRU5ErkJggg==";
-var image_box_g_1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACUAAAAhAgMAAAAjYYqGAAAACVBMVEUXwGgXrVuN2K6SNytBAAAAPklEQVR4AWNgDGAIDQ11YAAC7MwQBvxM6pgAAriZIWBSawV2JkwtVwMDdiZYLZi5agF2JlwtEw4mQTcQ9gUArqopHd9Tkt4AAAAASUVORK5CYII=";
-var image_box_g9_1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAeCAMAAABzP0xhAAAAElBMVEX////5vgD811L7yiL/63b70DwmvKIKAAAAAXRSTlMAQObYZgAAAFhJREFUeNq10EsOACEIA1BBuP+VbSZ+SJx0VbvQNLyF0n5jZrW+Q/bF3XHqEBv3FVQh4uPjpGiPq4iI6rSIR46cRosyE9e16tMBdGi+qa76diK0fkcDoEMD6XIGghdHfWQAAAAASUVORK5CYII=";
-var image_box_smile_1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAiAgMAAABKN5MWAAAACVBMVEX0tmHoplb/xYB7mtRGAAAAR0lEQVR4AZ3KoRUCQAwFsJiaTtcRT3dKgIf4j1PgIqIAFZqPJvTT2z0VmjeeDBX90oaG/VbdMvpSpf540KHi0rAHGyp66dADgalRpd5UfbgAAAAASUVORK5CYII=";
-var image_heart =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAGCAYAAAAPDoR2AAAAMElEQVR4AWO4HRICwv9BGJ0N5vyfOgWMgWwUDJcE0hgY3dj/6MZiU8AAk0RTgOADAGHXXFPSpAdaAAAAAElFTkSuQmCC";
-var image_bg1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGYAAAAMAQMAAACqflodAAAABlBMVEWlpaX///8ueuZRAAAAAnRSTlP/AOW3MEoAAAAoSURBVHgBY0AF/5HAHyYUKbJ5H2BmY+E9gPL+gXkHoLw/KG55D3ILABqxIVtE3VaRAAAAAElFTkSuQmCC";
-var image_bg2 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGYAAAAMAQMAAACqflodAAAABlBMVEWlpaX///8ueuZRAAAAAnRSTlP/AOW3MEoAAAArSURBVHgBY0AF/5HAHyYUKbJ5H6CM/Vh4D6DW1oGpeqjN30G2w93yHsQDAOuNLASIMDB0AAAAAElFTkSuQmCC";
-var image_bg3 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGYAAAAMAQMAAACqflodAAAABlBMVEWlpaX///8ueuZRAAAAAnRSTlP/AOW3MEoAAAARSURBVHgBY0AF/5HAn8HEAwDR9I5RjD0moAAAAABJRU5ErkJggg==";
-var image_smile_walk1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8//qrLi4uIZGRnTXl4zyFGCAAAAAXRSTlMAQObYZgAAAG9JREFUeNrt08EOgDAIA1An8P+/7AJxjY3crPFgTxt7S3pho4wzmOCqoOz2jJnlM4bQegqdmYeavE2v0VNoayKlWYW1u6/DnOsoXF9XRuHGSlL6EBEiWh1u+9H2Kmg5JqDIZBqKrqjbrMrj9M83cgB0DghxEx4eNAAAAABJRU5ErkJggg==";
-var image_smile_walk2 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8//qrLi4uIZGRnTXl4zyFGCAAAAAXRSTlMAQObYZgAAAG5JREFUeNrN00EOgCAMRFG17f2vbMNEJhYMq1H/Cuoj6cbtb+1XZSKj1R0tM+uf85wTaj2lboFmb9N7ekptD+ko9hi1u/dDzjUUbr2umkIg0PIgIkQUO0z3K3+vgsJVQsp0FBp3mwUgoquIVPT7TolyCIN83JMHAAAAAElFTkSuQmCC";
-var image_smile_walk3 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8//qrLi4uIZGRnTXl4zyFGCAAAAAXRSTlMAQObYZgAAAGZJREFUeNrt0EEKgDAMRFHbJPe/smXAjDbrCV34F1LjKwSvv9MaT9tERKubyMzwmUNqPaVG6zBRN/2mp9RWw94iyiWKdvc8rLmGQqPxykoyyjaxvUZEG6VGFCrK8pflrm203smnmN4Y1wb3hTd0dQAAAABJRU5ErkJggg==";
-var image_smile_walk4 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8//qrLi4uIZGRnTXl4zyFGCAAAAAXRSTlMAQObYZgAAAFxJREFUeNrt0zEOACEIBdFV4P5XXqJZohPbv5VTEfLs5LndUPvCRkbp+sjMcl6X0GpaOsthbv6me0pKbadyr6fU7l4DjJ4yPSXCg4iQ0nkStodbFdE+qhl/NBPTFwkABtN37E6XAAAAAElFTkSuQmCC";
-var image_smile_jump1 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8//qrLi4uIZGRnTXl4zyFGCAAAAAXRSTlMAQObYZgAAAGdJREFUeNrtkzEKwDAMA5sm+f+XK1JytPYs06E3BCHOoCXHz9dom9C4Vdpz0XtXfpZ6K1VsoXA31eobq8oF4zLqTSoeYI8xCOpL1USB2hZ44WDOaVLZkCeGv+pT8QJ4VpW5bUNW71cvSp8HWsP6+W0AAAAASUVORK5CYII=";
-var image_smile_jump2 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8/i4uL/qrIZGRnTXl7wwla7AAAAAXRSTlMAQObYZgAAAG5JREFUeNrF1EEKwCAMBVHbxvtfuSGBTqh1+dNZibyACDpeHdEgUlLcGTHAsJTirih13UT30Ch3eih6k5T6UT61mdVFC0XX0mkoDhr9RR9UB+acDRRHPF0NRXurA6ko7Zye4tYfs43mTXm+ADXRGw0/B1r0AxHCAAAAAElFTkSuQmCC";
-var image_smile_jump3 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8/i4uL/qrIZGRnTXl7wwla7AAAAAXRSTlMAQObYZgAAAG1JREFUeNq91EEKwCAMBVHbmvtfuSFSJjRk+TMrkZeNouvXFS0iJcXdEQMMSynuiY7Om+gZGp2dGYpuUtJW773zQkyrJpyM+n1AoyFa3YfygJlpaOOIp6uhaK86kIpS5/QUV3/MMXpOyvMFaIi+LxIHJO4t/1QAAAAASUVORK5CYII=";
-var image_smile_jump4 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAADPz8/i4uL/qrIZGRnTXl7wwla7AAAAAXRSTlMAQObYZgAAAGlJREFUeNq91DEKACEMRFF3E+9/5YWx+GElZeZXQV4aQdevRy2iSYp7FQssj1JcqKPrIdpD1TnxUHSTgd46M+tgoegazkOVjUZE82ZY2HuPUTSOeLpjFH070Dzt3DzF3T+mgXJTSgPIQj+jywbuBklzCwAAAABJRU5ErkJggg==";
-var image_smile_jump5 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAYAAADFw8lbAAAAqElEQVR4Ae3WAQbAMAwF0NJdopfrAQfYRYrdoBfpQCgSyk/E+OHDRL3yTQuHA84YY2nJlxEajJtzalHQCRcmVIkbbt2Pmn0HOJPQf0Ot/u07hOZDAbRXYv+jRgg1+wGgW2uSox1CY6EALrq7hEZCkS4aERyet3cJof5Qq6MRiJNLFnsIjYN6dQt5a2ZDCQV6CYCyoITuhyI4gJAPJdT87jS11kuSASX0A8DNP2HcPmxPAAAAAElFTkSuQmCC";
-var image_smile_jump6 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEX/qrL/qrLPz8/i4uIZGRnTXl4h7nIlAAAAAXRSTlMAQObYZgAAAHFJREFUeNrtkzEOwCAMA3GB/3+5liysCIZ2sbr0Bgj2ZaQRAO0FCdXer1auRU3Sak177wx5claiOaTqIi6wYFWXc6phqsKqnlFVkXF9ElLPqNpjDA8hFYCKR0Kq2Gw9azjnjKpeIJvdTF41/rQagur33F67CYiG+5KCAAAAAElFTkSuQmCC";
-var image_smile_jump7 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAMAAADyHTlpAAAAElBMVEUAAAD/qrLPz8/i4uIZGRnTXl481A+7AAAAAXRSTlMAQObYZgAAAG1JREFUeNrtkzEOwDAIA0sh//9yLVmxUBg6eWpvSIh9jLl+QER8WZ3evemJW+1pZiLEiZkJZ5PKC6iIDaq+7FMFUhZS+bSqjITqiUnNxrSrSoNJjQgWr5hUcth89nCtZVW1AA5brU+d6NNycKoPV+4Jf8Aga60AAAAASUVORK5CYII=";
-
-import Phaser from "phaser";
+// groups
+var blocks;
+var boxes;
+var platforms;
+var hearts;
 
 const config = {
   type: Phaser.AUTO,
   width: Math.min(Math.max(320, window.screen.width), 640),
   height: 208,
-  backgroundColor: "#ffffff",
+  backgroundColor: "#fafafa",
   parent: "gameframe",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 1000 },
+      debug: true
+    }
+  },
   scene: {
     preload: preload,
     create: create,
@@ -70,35 +45,115 @@ const config = {
   }
 };
 
-const game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
+
+//Data URI image
+var image_box_a_1 = "assets/box_a_1.png";
+var image_box_g_1 = "assets/box_g_1.png";
+var image_box_g9_1 = "assets/box_g9_1.png";
+var image_box_smile_1 = "assets/box_smile_1.png";
+var image_heart = "assets/heart.png";
+var image_bg1 = "assets/bg1.png";
+var image_bg2 = "assets/bg2.png";
+var image_bg3 = "assets/bg3.png";
+var image_smile_walk1 = "assets/smile_walk1.png";
+var image_smile_walk2 = "assets/smile_walk2.png";
+var image_smile_walk3 = "assets/smile_walk3.png";
+var image_smile_walk4 = "assets/smile_walk4.png";
+var image_smile_jump1 = "assets/smile_jump1.png";
+var image_smile_jump2 = "assets/smile_jump2.png";
+var image_smile_jump3 = "assets/smile_jump3.png";
+var image_smile_jump4 = "assets/smile_jump4.png";
+var image_smile_jump5 = "assets/smile_jump5.png";
+var image_smile_jump6 = "assets/smile_jump6.png";
+var image_smile_jump7 = "assets/smile_jump7.png";
 
 function preload() {
-  // "this" === Phaser.Scene
-  this.load.image(
-    "repeating-background",
-    "../assets/images/escheresque_dark.png"
-  );
+  // 'this' === Phaser.Scene
+  this.load.image("box_0", image_box_a_1);
+  this.load.image("box_1", image_box_g_1);
+  this.load.image("box_2", image_box_g9_1);
+  this.load.image("box_3", image_box_smile_1);
+  this.load.image("heart", image_heart);
+  this.load.image("bg1", image_bg1);
+  this.load.image("bg2", image_bg2);
+  this.load.image("bg3", image_bg3);
+  this.load.image("walk1", image_smile_walk1);
+  this.load.image("walk2", image_smile_walk2);
+  this.load.image("walk3", image_smile_walk3);
+  this.load.image("walk4", image_smile_walk4);
+  this.load.image("jump1", image_smile_jump1);
+  this.load.image("jump2", image_smile_jump2);
+  this.load.image("jump3", image_smile_jump3);
+  this.load.image("jump4", image_smile_jump4);
+  this.load.image("jump5", image_smile_jump5);
+  this.load.image("jump6", image_smile_jump6);
+  this.load.image("jump7", image_smile_jump7);
 }
 
 function create() {
   // You can access the game's config to read the width & height
   const { width, height } = this.sys.game.config;
 
-  // Creating a repeating background sprite
-  const bg = this.add.tileSprite(0, 0, width, height, "repeating-background");
-  bg.setOrigin(0, 0);
+  //make group
+  // blocks = this.add.group();
+  // boxes = this.add.group();
+  // platforms = this.add.group();
+  // hearts = this.add.group();
 
-  // In v3, you can chain many methods, so you can create text and configure it in one "line"
-  this.add
-    .text(width / 2, height / 2, "hello\nphaser 3\ntemplate", {
-      font: "175px monospace",
-      color: "white"
-    })
-    .setOrigin(0.5, 0.5)
-    .setShadow(5, 5, "#5588EE", 0, true, true);
+  this.anims.create({
+    key: "walk",
+    frames: [
+      { key: "walk1" },
+      { key: "walk2" },
+      { key: "walk3" },
+      { key: "walk4" }
+    ],
+    frameRate: 12,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "jump",
+    frames: [
+      { key: "jump1" },
+      { key: "jump2" },
+      { key: "jump3" },
+      { key: "jump4" },
+      { key: "jump5" },
+      { key: "jump6" },
+      { key: "jump7" }
+    ],
+    frameRate: 12,
+    repeat: 0
+  });
+
+  player = this.physics.add.sprite(72, 20, "jump7").play("jump");
+  ground = this.add.zone(width / 2, height - 20).setSize(width, 20);
+  this.physics.world.enable(ground, 1); // (0) DYNAMIC (1) STATIC
+
+  this.input.mouse.disableContextMenu();
 }
 
 function update(time, delta) {
+  // var touching = zone.body.touching;
+  // var wasTouching = zone.body.wasTouching;
+
+  // if (touching.none && !wasTouching.none) {
+  //   zone.emit("leavezone");
+  // } else if (!touching.none && wasTouching.none) {
+  //   zone.emit("enterzone");
+  // }
   // We aren't using this in the current example, but here is where you can run logic that you need
   // to check over time, e.g. updating a player sprite's position based on keyboard input
+  // zone.body.debugBodyColor = zone.body.touching.none ? 0x00ffff : 0xffff00;
+  this.physics.world.collide(player, ground, function() {});
+
+  this.input.on(
+    "pointerup",
+    function(pointer) {
+      // console.log("up");
+    },
+    this
+  );
 }
